c0163df9a95a7e33b5a41e75ee8eba93ec3418c7
Kamol774 <chust1222@gmail.com>
2024-06-04 12:52:28 +0500

    feat: organize and test pages router sytem

---
+++ pages/community/index.tsx
@@ -0,0 +1,14 @@
+const Community = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+
+  return (
+    <div>
+      COMMUNITY{" "}
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        PressMe
+      </button>
+    </div>
+  );
+};
+
+export default Community;
